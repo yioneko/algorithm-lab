8c1b9a505f50ddea1f5b411ded1b6846c8e2c933
yioneko <65551246+yioneko@users.noreply.github.com>
2021-06-11 04:05:25 +0000

    Deploying to gh-pages from @ yioneko/algorithm-lab@981b1b84ca5c68feee0f554688166e0db5cd682f 🚀

--- alg-lab-6/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export function useToggle(initialValue: boolean): [boolean, (newValue?: boolean) => void] {
-  const [state, setState] = useState(initialValue);
-  return [
-    state,
-    (newValue?: boolean) => setState((prev) => (newValue !== undefined ? newValue : !prev)),
-  ];
-}
